b0d0f4757de22ba3c8c6be5dc967cb1ce32fe25b
QianW2018 <43798833+QianW2018@users.noreply.github.com>
2018-11-13 13:44:48 -0500

    Add files via upload

---
+++ 1.1_view menu page/view_menu.js
@@ -0,0 +1,16 @@
+function startANewOrder(){
+
+	window.open("../1.2_menu page/menu.html");
+}
+
+function placeOrder(){
+	
+	window.open("../1.3_delivery address page/delivery_address.html");
+	
+}
+
+
+
+
+
+
