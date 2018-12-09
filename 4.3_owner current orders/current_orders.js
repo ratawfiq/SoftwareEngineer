b0e48c3b21ab00ed4fa6d612a373aa1fdda0bfee
ericjamescai <43798807+ericjamescai@users.noreply.github.com>
2018-12-09 15:28:32 -0500

    Add files via upload

--- 4.3_owner current orders/current_orders.js
+++ 4.3_owner current orders/current_orders.js
@@ -1,3 +1,5 @@
+//Created by Eric Cai
+
 function convertTime(time){
 	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 	var b=new Date(time*1000);
@@ -28,4 +30,64 @@ function convertString(val){
     +"\nComments:   "+val.Comments;
 
     return str;
+}
+
+function getDatabase(url){
+    //Do database call to get order header info
+    var details="";
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.open("GET", url, false);
+    oReq.send();     
+    if(oReq.status==200) {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        details=oReq.responseText;
+    };
+
+    return details; //Gives back order details in a String. Need to be parsed.
+    
+}
+
+function translateFood(foodID){
+    var foodName='';
+    switch(foodID){
+            case '1': 
+                foodName="Classic Quarter Chicken Dinner";
+                break;
+            case '2': 
+                foodName="Half Chicken Dinner";
+                break;
+            case '3': 
+                foodName= "Double Leg Dinner";
+                break;
+            case '4': 
+                foodName="Quarter Chicken & Shrimp Dinner";
+                break;
+            case '5': 
+                foodName="VEGGIE";
+                break;
+            case '6': 
+                foodName="BBQ CHICKEN";
+                break;
+            case '7': 
+                foodName="HAWAIIAN Beef";
+                break;
+            case '8': 
+                foodName="CRISPY CHICKEN";
+                break;
+            case '9': 
+                foodName="GOURMET CHEESEBURGER";
+                break;
+            default: 
+                foodName="NULL";
+            }
+    return foodName;
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
 }
