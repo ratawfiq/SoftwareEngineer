70cb762198e08f660d7fce7131bcc5cd0a3258b8
ericjamescai <43798807+ericjamescai@users.noreply.github.com>
2018-12-09 19:58:33 -0500

    Add files via upload

--- 4.2_owner previous orders/previous_orders.js
+++ 4.2_owner previous orders/previous_orders.js
@@ -1,3 +1,5 @@
+//Created by Eric Cai
+
 function convertTime(time){
 	var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
 	var b=new Date(time*1000);
@@ -15,17 +17,77 @@ function convertTime(time){
 function convertString(val){
 
 	var str="OrderID:   "+val.OrderID
-    +"\nUsername:   "+val.Username
-    +"\nFirst and Last Name: "+val.DeliveryFirstName+" "+val.DeliveryLastName
-    +"\nDelivery Location:   "+val.DeliveryAddress
+    +"</br>Username:   "+val.Username
+    +"</br>First and Last Name: "+val.DeliveryFirstName+" "+val.DeliveryLastName
+    +"</br>Delivery Location:   "+val.DeliveryAddress
     +" "+val.DeliveryCity
     +" "+val.DeliveryState
     +" "+val.DeliveryZipCode
-    +"\nPhone Number:  "+val.DeliveryPhone
-    +"\nOrder Submission Time:   "+convertTime(val.OrderSubmissionTime)
-    +"\nTotal Price:   "+val.TotalPrice
-    +"\nEstimated Delivery Time:   "+convertTime(val.InitialEstimatedDeliveryTime)
-    +"\nComments:   "+val.Comments;
+    +"</br>Phone Number:  "+val.DeliveryPhone
+    +"</br>Order Submission Time:   "+convertTime(val.OrderSubmissionTime)
+    +"</br>Total Price:   "+val.TotalPrice
+    +"</br>Estimated Delivery Time:   "+convertTime(val.InitialEstimatedDeliveryTime)
+    +"</br>Comments:   "+val.Comments +"</br>";
 
     return str;
+}
+
+function getDatabase(url){
+    //Do database call to get order header info
+    var details="";
+    var oReq = new XMLHttpRequest(); //New request object
+    oReq.open("GET", url, false);
+    oReq.send();     
+    if(oReq.status==200) {
+        //This is where you handle what to do with the response.
+        //The actual data is found on this.responseText
+        details=oReq.responseText;
+    };
+
+    return details; //Gives back order details in a String. Need to be parsed.
+    
+}
+
+function translateFood(foodID){
+    var foodName='';
+    switch(foodID){
+            case '1': 
+                foodName="Classic Quarter Chicken Dinner";
+                break;
+            case '2': 
+                foodName="Half Chicken Dinner";
+                break;
+            case '3': 
+                foodName= "Double Leg Dinner";
+                break;
+            case '4': 
+                foodName="Quarter Chicken & Shrimp Dinner";
+                break;
+            case '5': 
+                foodName="VEGGIE";
+                break;
+            case '6': 
+                foodName="BBQ CHICKEN";
+                break;
+            case '7': 
+                foodName="HAWAIIAN Beef";
+                break;
+            case '8': 
+                foodName="CRISPY CHICKEN";
+                break;
+            case '9': 
+                foodName="GOURMET CHEESEBURGER";
+                break;
+            default: 
+                foodName="NULL";
+            }
+    return foodName;
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
 }
